c18cf1d0375648744c453b4f1e7c895c93d1dd32
George MacRorie <me@georgemac.com>
2023-08-28 13:31:06 +0100

    feat(cup): add initial cup cli formula (v0.1.2)

---
+++ Formula/cup.rb
@@ -0,0 +1,26 @@
+class Cup < Formula
+  desc "A CLI for communicating with cupd"
+  homepage "https://cup.flipt.io"
+  license "apache-2.0"
+  head "https://github.com/flipt-io/cup/archive/refs/tags/v0.1.2.tar.gz"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cup`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/cup"
+  end
+end
