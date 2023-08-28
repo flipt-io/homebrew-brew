abeb945c2aee16ee73fd7ef00b815b5181c8780b
George MacRorie <me@georgemac.com>
2023-08-28 13:39:07 +0100

    fix(cup): add sha256 digest

--- Formula/cup.rb
+++ Formula/cup.rb
@@ -3,6 +3,7 @@ class Cup < Formula
   homepage "https://cup.flipt.io"
   license "apache-2.0"
   url "https://github.com/flipt-io/cup/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "8a92c7d849bbbba13f1e3c47918cb2cd13bc9b147433fa69e6dc968b99e3585a"
 
   depends_on "go" => :build
 
