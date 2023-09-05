21d60c72d1f11614e3b07e47b7683357816609a6
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-09-05 10:42:01 -0400

    chore: only install on arm macos for now

--- Formula/ffs.rb
+++ Formula/ffs.rb
@@ -3,13 +3,15 @@ require "formula"
 class Ffs < Formula
   desc "Flipt Flag Search"
   homepage "https://flipt.io"
-  version "0.0.17"
   license "Apache-2.0"
-  url "https://github.com/flipt-io/ffs/releases/download/v0.0.17/ffs-aarch64-apple-darwin.tar.gz"
-  sha256 "9c7ddbee01ea43bfb6d45c9d3b58fe0140f596d8c6e5244bb27e6d974fe2b541"
-  head "https://github.com/flipt-io/ffs.git"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/flipt-io/ffs/releases/download/v0.0.17/ffs-aarch64-apple-darwin.tar.gz"
+      sha256 "9c7ddbee01ea43bfb6d45c9d3b58fe0140f596d8c6e5244bb27e6d974fe2b541"
 
-  def install
-    bin.install "ffs"
+      def install
+        bin.install "ffs"
+      end
+    end
   end
 end
