3739f51bd0383fe4aba98fb1015e42c6b1554383
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-09-05 10:49:24 -0400

    chore: try to appease brew linter

--- Formula/ffs.rb
+++ Formula/ffs.rb
@@ -1,13 +1,11 @@
-require "formula"
-
 class Ffs < Formula
   desc "Flipt Flag Search"
   homepage "https://flipt.io"
-  version "v0.0.17"
+  version "0.0.17"
   license "Apache-2.0"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/flipt-io/ffs/releases/download/#{version}/ffs-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/flipt-io/ffs/releases/download/v#{version}/ffs-aarch64-apple-darwin.tar.gz"
       sha256 "9c7ddbee01ea43bfb6d45c9d3b58fe0140f596d8c6e5244bb27e6d974fe2b541"
 
       def install
