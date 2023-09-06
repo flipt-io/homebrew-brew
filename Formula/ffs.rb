de274fbef4094301c53a1e39df4a2c5cb7f2976c
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-09-06 09:45:54 -0400

    chore: update ffs to latest release (#3)

--- Formula/ffs.rb
+++ Formula/ffs.rb
@@ -1,12 +1,12 @@
 class Ffs < Formula
   desc "Flipt Flag Search"
   homepage "https://flipt.io"
-  version "0.0.17"
+  version "0.0.18"
   license "Apache-2.0"
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/flipt-io/ffs/releases/download/v#{version}/ffs-aarch64-apple-darwin.tar.gz"
-      sha256 "9c7ddbee01ea43bfb6d45c9d3b58fe0140f596d8c6e5244bb27e6d974fe2b541"
+      sha256 "a486e4e0890fc53b4cdb20ef16a94da6c464c1c19485133826ae9d4ae9cd2b87"
 
       def install
         bin.install "ffs"
